efd7642ddc037e067cdf381233610130f1452e72
Camilo <juancmaringcu@gmail.com>
2021-05-29 17:31:33 -0500

    finish

---
+++ angular-LocalStorage/src/app/services/task.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {Task} from '../models/task';
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  tasks: Task[];
+  constructor() { 
+    this.tasks = [
+      /* {title: "Leer", description: "Necesito Leer", hide:true},
+      {title: "Correr", description: "Necesito Correr", hide:true} */
+    ];
+  }
+  getTask(){
+    if(localStorage.getItem('tasks')  === null){
+      return this.tasks; 
+    }else{
+      this.tasks = JSON.parse(localStorage.getItem('tasks'));
+      return this.tasks;
+    }
+    
+  }
+
+  addTask(task: Task){
+    this.tasks.push(task);
+    let tasks: Task[] = [];
+    if(localStorage.getItem('tasks')  === null){
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }else{
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+    
+    
+  }
+  deleteTask(task: Task){
+    for(let i = 0; i < this.tasks.length; i++){
+      if(task == this.tasks[i]){
+        this.tasks.splice(i, 1);
+        localStorage.setItem('tasks',JSON.stringify(this.tasks))
+      }
+    }
+  }
+
+}
